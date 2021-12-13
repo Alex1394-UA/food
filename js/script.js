fa6f3e9da5a7ac64bae0e1620d18dc8ca4c274b2
Alexander <homeholder@bigmir.net>
2021-12-14 00:35:56 +0200

    after lesson 59 cleared almost  all old commented codes

--- js/script.js
+++ js/script.js
@@ -118,8 +118,6 @@ window.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = '';
     }
 
-    //modalBtnClose.addEventListener('click', closeModal);
-
     modal.addEventListener('click', (e) => {
         if (e.target === modal || e.target.getAttribute('data-close') == '') {
             closeModal();
@@ -204,40 +202,6 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         });
 
-    //классический вариант:
-    /* const div = new MenuCard();
-    div.render(); */
-
-    //сокращённый вариант
-    /* new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu .container'
-    ).render();
-
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        14,
-        '.menu .container',
-        'menu__item'
-    ).render();
-
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        21,
-        '.menu .container',
-        'menu__item'
-    ).render(); */
-
     // FORMS
 
     const forms = document.querySelectorAll('form');
@@ -275,7 +239,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 display: block;
                 margin: 0 auto;
             `;
-            //form.append(statusMessage);
+            
             form.insertAdjacentElement('afterend', statusMessage);
 
             // заменяем на метод fetch()
@@ -285,28 +249,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
             const formData = new FormData(form);
 
-            // старый метод перевода formData в json
-            /* const object = {};
-            formData.forEach(function(value, key) {
-                object[key] = value;
-            }); */
-
             // новый метод перевода formData в json
             const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            //const json = JSON.stringify(object);
-
-            // request.send(json);
-            // ниже старый код
-            /* fetch('server.php', {
-                method: "POST",
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                //body: formData
-                body: JSON.stringify(object)
-            }) */
-            // выше старый код
             postData ('http://localhost:3000/requests', json)
             .then(data => {
                 console.log(data);
@@ -357,20 +302,4 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     } 
-
-    /* fetch('http://localhost:3000/menu')
-        .then(data => data.json())
-        .then(res => console.log(res)); */
 });
-
-// Пример fetch-запроса
-
-/* fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: "POST",
-        body: JSON.stringify({name: 'Alex'}),
-        headers: {
-            'Content-type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(json => console.log(json)); */

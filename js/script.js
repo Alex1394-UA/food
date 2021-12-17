63b8baf16e2ec589f537bdd0b67efa6c01648014
Alexander <homeholder@bigmir.net>
2021-12-18 01:23:21 +0200

    Regular Expressions lesson 65

--- js/script.js
+++ js/script.js
@@ -346,6 +346,7 @@ window.addEventListener('DOMContentLoaded', () => {
           slidesWrapper = document.querySelector('.offer__slider-wrapper'),
           slidesField = document.querySelector('.offer__slider-inner'),
           width = window.getComputedStyle(slidesWrapper).width;
+
     let slideIndex =1;
     let offset = 0;
 
@@ -411,12 +412,11 @@ window.addEventListener('DOMContentLoaded', () => {
         dots.push(dot);
     }
 
-
     next.addEventListener('click', () => {
-       if (offset == +width.slice(0, width.length -2) * (slides.length - 1)) {
+       if (offset == deleteNotDigits(width) * (slides.length - 1)) {
            offset = 0;
        } else {
-        offset += +width.slice(0, width.length -2);
+        offset += deleteNotDigits(width);
        }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -433,9 +433,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length -2) * (slides.length - 1);
+            offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-         offset -= +width.slice(0, width.length -2);
+         offset -= deleteNotDigits(width);
         }
  
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -455,7 +455,7 @@ window.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = offset = +width.slice(0, width.length -2) * (slideTo - 1);
+            offset = offset = deleteNotDigits(width) * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -464,6 +464,10 @@ window.addEventListener('DOMContentLoaded', () => {
          });
          
      });
+
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
 
      function twoDigitsIndex() {
         if (slides.length < 10) {
